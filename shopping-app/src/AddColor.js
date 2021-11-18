1f2ff68793171e79cb82106624d960bd1b1e6bbb
Chetan-charan <chetanharicharan@gmail.com>
2021-11-18 12:02:13 +0530

    Redux change color

--- shopping-app/src/AddColor.js
+++ shopping-app/src/AddColor.js
@@ -1,19 +1,39 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { ColorBox } from "./ColorBox";
 
+
+const initialState = {color: 'white'};
+
+const reducer = (state, action) => {
+
+  switch (action.type) {
+    case "changecolor":
+    
+    return { color: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 export function AddColor() {
+
+  const [state,dispatch] = useReducer(reducer,initialState);
 
   const [colors, setColors] = useState(['teal', 'orange', 'lavender']);
 
 
-  let [color, setColor] = useState('white');
-  const Styles = { backgroundColor: color };
+  
+  const Styles = { backgroundColor: state.color };
   return <div>
     <div className='Add-color'>
-      <TextField value={color} style={Styles} onChange={(event) => setColor(event.target.value)} id="filled-basic" label="Enter a color" variant="filled" />
-      <Button onClick={() => setColors([...colors, color])} variant="contained">Add color</Button>
+      
+      <TextField value={state.color} style={Styles} onChange={(event) => dispatch({type: 'changecolor',payload:event.target.value})} id="filled-basic" label="Enter a color" variant="filled" />
+      
+      
+      <Button onClick={() => setColors([...colors, state.color])} variant="contained">Add color</Button>
       {colors.map((clr, index) => <ColorBox color={clr} key={index} />)}
     </div>
   </div>;
